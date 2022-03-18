fd4a939b0a9add20cbfc05a9fefe62031a033b85
Daniel Silva <daniel.silva@onlineapp.com.br>
2022-03-18 18:49:36 -0300

    List

---
+++ Course-Alura/C#/ListAndArray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListAndArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] number = { 21, 65, 43, 1, 54, 2, 76, 3, 4, 5, 6, 7 };
+
+            List<string> name = new List<string>();
+            name.Add("Daniel");
+            name.Add("Pedro");
+            name.Add("Fernando");
+            name.Add("Fernando");
+
+            Array.Sort(number);
+
+            foreach(int v in number)
+            {
+                Console.Write(v +" ");
+            }
+
+            foreach (string l in name)
+            {
+                Console.Write(l + " ");
+            }
+
+            Console.WriteLine(name.Count);
+            Console.WriteLine(name.Exists(e => e == "Daniel"));
+            Console.WriteLine(name.Exists(e => e.IndexOf("Dani") == -1)); // buscando por parte a string. -1 e true
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
--- Course-Alura/angular/project-angular/src/app/app.module.ts
+++ Course-Alura/angular/project-angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransferenciaComponent } from './component/transferencia/transferencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransferenciaComponent
   ],
   imports: [
     BrowserModule,
