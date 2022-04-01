e8142b84aed28b2b9aae5c8aed3ec8902a54a418
Daniel Silva <daniel.silva@onlineapp.com.br>
2022-04-01 09:04:46 -0300

    aspnet

---
+++ Course-Alura/MovieFlix/Controllers/MovieController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieFlix.Models;
+
+namespace MovieFlix.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+
+        private static List<Movie> movies = new List<Movie>();
+        private static int id = 1;
+
+        [HttpPost]
+        public void addMovie([FromBody] Movie movie)
+        {
+            movie.Id = id++;
+            movies.Add(movie);
+
+        }
+
+
+        [HttpGet]
+        public IActionResult GetMovies()
+        {
+            return Ok(movies);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMovieById(int id)
+        {
+            
+           Movie movie = movies.FirstOrDefault(m => m.Id == id); // modo usual
+           if(movie != null)
+           {
+              return Ok(movie);
+           }
+               
+           return NotFound();
+          
+        }
+    }
+}
---
+++ Course-Alura/MovieFlix/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieFlix.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "O campo é obrigatorio")]
+        public string Name {get; set;}
+
+        [StringLength(30, ErrorMessage = "O genero não pode passa de 30 caracteres")]
+        [Required]  // é obrigatorio
+        public string Genre { get; set; }
+
+        [Range(1, 600)] // vai dura no minimo 1 maxino 600
+        public int Duration { get; set; }
+
+       
+    }
+}
